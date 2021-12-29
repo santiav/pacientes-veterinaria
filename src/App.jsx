5f241cdc7bd5bdd55208fb790410d6874bf9cb9e
Santiago Acosta <santiav@gmail.com>
2021-12-29 12:04:09 -0300

    almacenar y leer desde localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from './components/Header'
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -11,8 +11,18 @@ function App() {
     // Paciente seleccionado al editar 
     const [ pacienteSeleccionado, setPacienteSeleccionado] = useState({});
 
+    useEffect(() => {
+        /* cuando el parámetro es un arreglo vacio, solo se carga una vez */
+        let pacientesExistentes = JSON.parse(localStorage.getItem('pacientes')) ?? []
+        setPacientesAgregados(pacientesExistentes)
+    }, [])
+
+    // actualizar el localstorage
+    useEffect(() => {
+        localStorage.setItem('pacientes', JSON.stringify(pacientesAgregados))
+    }, [pacientesAgregados])
+
     const eliminarPaciente = id => {
-        console.log("eliminando paciente", id)
         const pacientesActualizados = pacientesAgregados.filter( item => item.id !== id)
         setPacientesAgregados(pacientesActualizados)
     }
