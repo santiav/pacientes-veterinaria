4e675145273e928f299c646c19e95d8e9281f9d5
Santiago Acosta <santiav@gmail.com>
2021-12-28 21:17:48 -0300

    Renderización condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,22 +1,31 @@
 import Paciente from "./Paciente"
 
 const ListadoPacientes = ({pacientesAgregados}) => {
+
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
+
             <h2 className="text-3xl text-center">Listado de Pacientes</h2>
+
             <p className="text-lg mt-5 text-center mb-10">
                 Administra tus {''}
                 <span className="text-indigo-600 font-bold">Pacientes y citas</span>
             </p>
 
-            {pacientesAgregados.map( item => {
-                return (
-                    <Paciente 
-                        key={item.id}
-                        paciente={item}
-                    />
-                )
-            })}
+
+            {pacientesAgregados && pacientesAgregados.length ? (
+
+                <>
+                    {pacientesAgregados.map( item => {
+                        return (
+                            <Paciente 
+                                key={item.id}
+                                paciente={item}
+                            />
+                        )
+                    })}
+                </>
+            ) : "No hay pacientes"}
             
         </div>
     )
